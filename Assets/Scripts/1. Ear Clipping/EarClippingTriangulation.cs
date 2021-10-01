789f308b1ec1b5de0b300ef87fcc095a6058e4a2
Nirmal <lynchamika@gmail.com>
2021-10-01 07:21:49 +0530

    +basic implementation done

---
+++ Assets/Scripts/1. Ear Clipping/EarClippingTriangulation.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+ 
+public class EarClippingTriangulation : MonoBehaviour
+{
+    #region PROPERTIES
+    public static EarClippingTriangulation Instance { get; private set; }
+    [SerializeField] private Transform[] vertexTArray;
+
+    internal List<Vector2>  vertexVList { get; private set; }
+    private List<Vector2> tempVertexVList;
+    internal List<int> trianglePointIndexList { get; private set; }
+    #endregion
+
+    #region UNITY CALLBACKS
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        vertexVList = vertexTArray.Select(t => new Vector2(t.position.x,t.position.z)).ToList();//VERTEX COUNT -> n
+
+        tempVertexVList = new List<Vector2>();
+        tempVertexVList.AddRange(vertexVList);
+
+        trianglePointIndexList = new List<int>();//TRIANGLE COUNT -> (n-2)
+
+        FindAllTriangles();
+        MeshGenarator.Instance.GenarateMesh();
+    }
+    #endregion
+
+    #region EAR CLIPPING METHODS
+    //https://arxiv.org/ftp/arxiv/papers/1212/1212.6038.pdf
+    private void FindAllTriangles()
+    {
+
+        while(tempVertexVList.Count > 2)
+        {
+            for(int i=0;i< tempVertexVList.Count;i++)
+            {
+                Vector2 _a = tempVertexVList[GetNextIndex(i)];
+                Vector2 _b = tempVertexVList[i];
+                Vector2 _c = tempVertexVList[GetPreviousIndex(i)];
+
+                if(IsConvexVertex(_a,_b,_c))
+                {
+                    if(IsAValidEar(_a,_b,_c))
+                    {
+                        tempVertexVList.Remove(_b);
+                        trianglePointIndexList.Add(vertexVList.IndexOf(_b));
+                        trianglePointIndexList.Add(vertexVList.IndexOf(_a));
+                        trianglePointIndexList.Add(vertexVList.IndexOf(_c));
+                        break;
+                    }
+                }
+            
+            }
+        }
+
+        //Debug.Log(trianglePointIndexList.Count);
+    }
+
+
+    //USING DETERMINATE
+    //https://stackoverflow.com/questions/40410743/polygon-triangulation-reflex-vertex
+    private bool IsConvexVertex(Vector2 a, Vector2 b, Vector2 c)
+    {
+        //CONVEX VERTEX - VETREX ANGLE IS LESS THAN 180 DEGRESS.
+
+        //The determinate gives us as result greater zero if the vertices form a left turn, which means that three consecutive vertices a,b, and c form a convex angle at b; and less than zero otherwise.
+        //(b.x - a.x) * (c.y - b.y) - (c.x - b.x) * (b.y - a.y) > 0
+
+        float d1 = (b.x - a.x) * (c.y - b.y);
+        float d2 = (c.x - b.x) * (b.y - a.y);
+
+        if(d1 - d2 > 0)
+        {
+            return true;
+        }
+        //EDGE CASE - 180
+
+        return false;
+    }
+
+    private int GetNextIndex(int index)
+    {
+        //return index + 1 < vertexList.Count ? index + 1 : (index + 1) % vertexList.Count; 
+        return index + 1 < tempVertexVList.Count ? index + 1 : 0;
+    }
+
+    private int GetPreviousIndex(int index)
+    {
+        return index - 1 >= 0 ? index - 1 : tempVertexVList.Count-1;
+    }
+
+   
+
+    private bool IsAValidEar(Vector2 a, Vector2 b, Vector2 c)
+    {
+        for (int i = 0; i < tempVertexVList.Count; i++)
+        {
+            if (tempVertexVList[i] != a || tempVertexVList[i] != b || tempVertexVList[i] != c)
+            {
+                if(IsPointInTriangle(a,b,c,tempVertexVList[i]))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    //https://blackpawn.com/texts/pointinpoly/
+    private bool IsPointInTriangle(Vector2 a, Vector2 b, Vector2 c,Vector2 p)
+    {
+        bool bool1 = IsSameSide(a, b, c, p);
+        bool bool2 = IsSameSide(b, c, a, p);
+        bool bool3 = IsSameSide(c, a, b, p);
+
+        if(bool1 && bool2 && bool3)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsSameSide(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4)
+    {
+        float f1 = CrossProductSignedMagnitude(p1 - p2,p3 - p2);
+        float f2 = CrossProductSignedMagnitude(p1 - p2, p4 - p2);
+
+        //IF HAS THE SAME SIGN, THEN THEY ARE IN THE SAME SIDE
+        if(f1 < 0)
+        {
+            if(f2 < 0)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            if (f2 > 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private float CrossProductSignedMagnitude(Vector2 a, Vector2 b)
+    {
+        return (a.x * b.y - a.y * b.x);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Mesh Genaration/MeshGenarator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+ 
+public class MeshGenarator : MonoBehaviour
+{
+    #region PROPERTIES
+    public static MeshGenarator Instance { get; private set; }
+    Mesh mesh;
+    #endregion
+
+    #region UNITY CALLBACKS
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    #endregion
+
+    #region UTIL
+    internal void GenarateMesh()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+        //mesh = GetComponent<MeshFilter>().sharedMesh;
+
+        List<Vector3> vertsList = new List<Vector3>();//VERTICES LIST
+        List<int> triList = new List<int>();//TRIANGLE LIST
+        List<Vector3> normalList = new List<Vector3>(); //NORMAL LIST (WE NEED NORMALS IF NEED LIGTING TO WORK ON THE MESH)
+        List<Vector2> uvList = new List<Vector2>();//MAP TEXTURES
+
+        AddVertices(vertsList, normalList, uvList);
+        AddTriangles(triList);
+
+        //mesh.vertices = vertsList.ToArray();//SETTING PROPERTIES MAKE MORE GARBAGE
+        //mesh.triangles = triList.ToArray();
+        //mesh.normals = normalList.ToArray();
+
+        mesh.SetVertices(vertsList);
+        mesh.triangles = triList.ToArray();
+        mesh.SetNormals(normalList);
+        //mesh.SetUVs(0, uvList);
+
+        //mesh.RecalculateNormals();//THIS IS AN EXPENSIVE OPERATION. SHOULD NOT BE USED WHEN THE MESH IS GENAREATED AGAIN ANF AGAIN.
+    }
+
+    private void AddVertices(List<Vector3> vertsList, List<Vector3> normalList, List<Vector2> uvList)
+    {
+        
+        foreach(Vector2 v in EarClippingTriangulation.Instance.vertexVList)
+        {
+            vertsList.Add(new Vector3(v.x,0,v.y));
+            normalList.Add(Vector3.down);
+        }
+
+        //uvList.Add(new Vector2(0, 0));
+        //uvList.Add(new Vector2(0, 1));
+        //uvList.Add(new Vector2(1, 0));
+        //uvList.Add(new Vector2(1, 1));
+    }
+
+    private void AddTriangles(List<int> triList)
+    {
+        //LEFT HAND COORDINATE SYSTEM
+        //WINDING ORDER IS IMPORATANT. IN A QUAD ONLY ONE SIDE WILL GET RENDRED.
+
+        foreach (int index in EarClippingTriangulation.Instance.trianglePointIndexList)
+        {
+            Debug.Log(index);
+            triList.Add(index);
+        }
+    }
+    #endregion
+}
